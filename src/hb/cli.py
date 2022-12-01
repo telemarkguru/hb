92c0b26faf30a82ee9f10b5ef3189e42e02ea003
Stefan Sandström <stefans@axis.com>
2022-12-01 14:07:10 +0900

    Use click for command line parsing

--- src/hb/cli.py
+++ src/hb/cli.py
@@ -1,11 +1,18 @@
 from ._path import pathset, relative, cwd
-import sys
+import click
 
 
-def explist():
-    paths = pathset(sys.argv[1:], anchor=cwd())
-    for p in relative(cwd(), paths):
-        print(p)
+@click.command()
+@click.option("-a", "--absolute", help="Output absolute paths")
+@click.argument("listfiles", nargs=-1)
+def explist(absolute, listfiles):
+    paths = pathset(listfiles, anchor=cwd())
+    if absolute:
+        for p in paths:
+            print(p)
+    else:
+        for p in relative(cwd(), paths):
+            print(p)
 
 
 def main():
